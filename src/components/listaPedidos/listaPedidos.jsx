efdde86d46a199d1534ded986e67c2b534c71b5a
iagoam97 <mothearendtiago@gmail.com>
2022-03-14 09:30:24 -0300

    Ajute na seleção em DOM dos sabores para o carrinho

--- src/components/listaPedidos/listaPedidos.jsx
+++ src/components/listaPedidos/listaPedidos.jsx
@@ -1,13 +1,43 @@
 import React from "react";
-import './listaPedidos.css'
+import ReactDOM from 'react-dom'
+import './listaPedidos.css';
 
 import {listSabores} from '../listaSabores/listSabores.js';
 
 function listaPedidos() {
 
+    const arrCarrinho = [];
+
     function SelectSabor(e) {
-        const idSelecionaSabor = e.target
-        console.log(idSelecionaSabor)
+        const idSelecionaSabor = e.target.id;
+        const getID = idSelecionaSabor.substr(idSelecionaSabor.length - 3);
+
+        const imgPizzaSelecionada = e.target.parentNode.parentNode.firstChild.src
+        const nomePizzaSelecionada = e.target.parentNode.childNodes[0].textContent
+        const valorPizzaSelecionada = e.target.parentNode.childNodes[2].textContent
+
+        arrCarrinho.push({idCarr: getID, nomeCarr: nomePizzaSelecionada, vlrCarr: valorPizzaSelecionada, imgCarr: imgPizzaSelecionada})
+
+        let getVlrCarrinho = document.getElementById("valorTotalCarrinho").textContent
+        let somaVlrCarrinho = parseFloat(getVlrCarrinho) + parseFloat(valorPizzaSelecionada)
+        document.getElementById("valorTotalCarrinho").innerHTML=somaVlrCarrinho.toFixed(2);
+
+        const retornoDiv = 
+            arrCarrinho.map(function(arrCarrinho, index) {
+                return (
+                    <div className="pizzaCarrinho" id={index}>
+                        <img className="imgPizzaCarrinho" src={arrCarrinho.imgCarr} alt={"Imagem da pizza sabor " + arrCarrinho.nomeCarr}/>
+                        <div className="descrPizzaCarrinho">
+                            <p>{arrCarrinho.nomeCarr}</p>
+                            <p className="formatarDinheiro">{arrCarrinho.vlrCarr}</p>
+                        </div>
+                    </div>
+                )
+            })
+
+        ReactDOM.render((
+            retornoDiv
+        ), document.getElementById('quadroSelecao'));
     }
 
     return (
@@ -15,24 +45,27 @@ function listaPedidos() {
             <div className="listaOpcoes">
                 {listSabores.map(function(sabor, index) {
                     return (
-                        <div className="infoSaboresPedido" onClick={SelectSabor}>
+                        <div className="infoSaboresPedido">
                             {<img src={sabor.image} alt='Pedidos pizza' className="imagemPedido"/>}
                             <div className="detalhesPedidos">
                                 <p className="nomePedidosPizza">{sabor.nome}</p>
                                 <p className="descrPedidosPizza">{sabor.ingredientes}</p>
-                                <p className="vlrPedidosPizza">{sabor.valor}</p>
-                                <button id="">Selecionar</button>
+                                <p className="vlrPedidosPizza formatarDinheiro">{sabor.valor}</p>
+                                <button id={"SaborID" + sabor.id} onClick={SelectSabor}>Adicionar</button>
                             </div>
                         </div>
                     )
                 })}
             </div>
             <div className="listaSelecionados">
-                <div className="quadroSelecao">
+                <div id="quadroSelecao">
                 </div>
-                <div className="btnsSelecao">
-                    <button id="btnEfetuarPedido">Efetuar Pedido</button>
-                    <button id="btnCancelarPedido">Limpar Pedido</button>
+                <div>
+                    <p id="valorTotalCarrinho" className="formatarDinheiro">0.00</p>
+                    <div className="btnsSelecao">
+                        <button id="btnEfetuarPedido">Efetuar Pedido</button>
+                        <button id="btnCancelarPedido">Limpar Pedido</button>
+                    </div>
                 </div>
 
             </div>
--- src/components/listaSabores/listSabores.js
+++ src/components/listaSabores/listSabores.js
@@ -9,49 +9,49 @@ import imgPizza4Carnes from '../../assets/sabores/pizza4Carnes.jpg';
 
 export const listSabores = [
     {
-        id: 1,
+        id: '001',
         nome: 'Quatro Queijos',
         ingredientes:'Molho de tomate, muçarela, provolone, parmesão, gorgonzola e oregano',
         valor: 39.99,
         image: imgPizza4Queijos
     },
     {
-        id: 2,
+        id: '002',
         nome: 'Frango Catupiry',
         ingredientes:'Molho de tomate, peito de frango desfiado, azeitona, muçarela, catupiry, tomate e oregano',
         valor: 35.99,
         image: imgPizzaFrango
     },
     {
-        id: 3,
+        id: '003',
         nome: 'Pepperoni',
         ingredientes:'Molho de tomate, pepperoni, muçarela, tomate e oregano',
         valor: 34.99,
         image: imgPizzaPepperoni
     },
     {
-        id: 4,
+        id: '004',
         nome: 'Presunto',
         ingredientes:'Molho de tomate, presunto, azeitona, muçarela, tomate e oregano',
         valor: 32.99,
         image: imgPizzaPresunto
     },
     {
-        id: 5,
+        id: '005',
         nome: 'Portuguesa',
         ingredientes:'Molho de tomate, ovos, azeitona, ervilha, muçarela, presunto e oregano',
         valor: 37.99,
         image: imgPizzaPortuguesa
     },
     {
-        id: 6,
+        id: '006',
         nome: 'Calabresa',
         ingredientes:'Molho de tomate, Calabresa, azeitona, cebola(muito pouco), muçarela, tomate e oregano',
         valor: 36.99,
         image: imgPizzaCalabresa
     },
     {
-        id: 7,
+        id: '007',
         nome: 'Quatro Carnes',
         ingredientes:'Molho de tomate, presunto, muçarela, calabresa fatiada, lombo canadense fatiado, bacon e oregano',
         valor: 44.99,
